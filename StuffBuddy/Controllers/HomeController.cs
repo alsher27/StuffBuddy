78b9bfdb46dde84d306a038e9c8bb570eb8e07fe
Alex Sherzhukov <alsher27@gmail.com>
2019-10-22 12:11:34 +0300

    Added WebUI Render

---
+++ StuffBuddy/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StuffBuddy.DAL;
+
+namespace StuffBuddy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        public HomeController(Context context)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+    
+}
